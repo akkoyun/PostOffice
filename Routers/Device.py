d97f09a0d327cb50cf8912aa6bd3a7d5e740ec7f
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-07-02 16:25:48 +0300

    Learn updates

--- Routers/Device.py
+++ Routers/Device.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import Body, FastAPI, APIRouter
 from pydantic import BaseModel
 
@@ -7,18 +8,28 @@ Device = APIRouter(
     tags=['Device Operations']
 )
 
+Sample_Device_List = [
+    {"Type" :101, "ID" : "00112233445566", "Location" : "Konya", "Owner" : "STF", "IP" : "1.1.1.1"},
+    {"Type" :102, "ID" : "00112233445566", "Location" : "Sarayonu", "Owner" : "STF", "IP" : "2.2.2.2"}
+    ]
+
 # Define Schema
 class Device_Post(BaseModel):
-    Type: int
+    Type: int = 101
     ID: str
-    Location: str
-    Owner: str
-    IP: str
+    Location: Optional[str] = "STF HQ"
+    Owner: Optional[str] = "STF"
+    IP: Optional[str] = "0.0.0.0"
 
 # Device End Point Default Request
 @Device.get("/")
 def Device_Root():
     return {"message": "Device End Point"}
+
+# Device List
+@Device.get("/List")
+def Device_List():
+    return {"Device_List": Sample_Device_List}
 
 # Device Create
 @Device.post("/Create")
