991abaca8caa4c16d36f6857ef34c43e571701f1
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-07 16:44:37 +0300

    consumer update

--- Consumer/Consumer_Buffer_RAW.py
+++ Consumer/Consumer_Buffer_RAW.py
@@ -11,34 +11,34 @@ Kafka_Consumer = KafkaConsumer(APP_Settings.KAFKA_TOPIC_RAW,
     group_id="Data_Consumer", 
     enable_auto_commit=True)
 
-async def Buffer_RAW():
+async def Record_Message():
 
-    # Handle All Message in Topic
-    for Message in Kafka_Consumer:
+    # Get Headers
+    Command = Message.headers[0][1].decode('ASCII')
+    Device_ID = Message.headers[1][1].decode('ASCII')
+    Device_IP = Message.headers[2][1].decode('ASCII')
 
-        # Get Headers
-        Command = Message.headers[0][1].decode('ASCII')
-        Device_ID = Message.headers[1][1].decode('ASCII')
-        Device_IP = Message.headers[2][1].decode('ASCII')
+    # handle Message
+    Kafka_Message = json.loads(Message.value.decode())
 
-        # handle Message
-        Kafka_Message = json.loads(Message.value.decode())
+    print("Command     : ", Command)
+    print("Device ID   : ", Device_ID)
+    print("Client IP   : ", Device_IP)
+    print(".........................................................")
 
-        print("Command     : ", Command)
-        print("Device ID   : ", Device_ID)
-        print("Client IP   : ", Device_IP)
-        print(".........................................................")
+    # Create Add Record Command
+    New_Buffer_Post = await Incoming_Buffer(Buffer_Device_ID = Device_ID, Buffer_Client_IP = Device_IP, Buffer_Command = Command, Buffer_Data = str(Kafka_Message))
 
-        # Create Add Record Command
-        New_Buffer_Post = Incoming_Buffer(Buffer_Device_ID = Device_ID, Buffer_Client_IP = Device_IP, Buffer_Command = Command, Buffer_Data = str(Kafka_Message))
+    # Add and Refresh DataBase
+    db = SessionLocal()
+    await db.add(New_Buffer_Post)
+    await db.commit()
+    await db.refresh(New_Buffer_Post)
 
-        # Add and Refresh DataBase
-        db = SessionLocal()
-        db.add(New_Buffer_Post)
-        db.commit()
-        db.refresh(New_Buffer_Post)
+    # Print LOG
+    print("Message recorded to Buffer DB with Buffer_ID : ", New_Buffer_Post.Buffer_ID)
+    print("---------------------------------------------------------")
 
-        # Print LOG
-        print("Message recorded to Buffer DB with Buffer_ID : ", New_Buffer_Post.Buffer_ID)
-        print("---------------------------------------------------------")
-
+# Handle All Message in Topic
+for Message in Kafka_Consumer:
+    Record_Message()
