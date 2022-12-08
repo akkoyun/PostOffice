6454de5c01e9dfca4b487141bd7ddb8d18a8ed94
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-08 14:41:29 +0300

    paydentic update

--- Consumer/Consumer_Buffer_RAW.py
+++ Consumer/Consumer_Buffer_RAW.py
@@ -1,9 +1,11 @@
 # Import Libraries
 from Config import APP_Settings
 from Database import SessionLocal, DB_Engine
-import Models
-from kafka import KafkaConsumer
+import Models, Schema
+from kafka import KafkaConsumer, KafkaProducer
+from kafka.errors import KafkaError
 import json
+from json import dumps
 
 # Create DB Models
 Models.Base.metadata.create_all(bind=DB_Engine)
@@ -15,56 +17,84 @@ Kafka_Consumer = KafkaConsumer(APP_Settings.KAFKA_TOPIC_RAW,
     auto_offset_reset='earliest',
     enable_auto_commit=False)
 
-def Record_Message():
+# Defne Kafka Producer
+Kafka_Producer = KafkaProducer(
+    value_serializer=lambda m: dumps(m).encode('utf-8'),
+    bootstrap_servers=f"{APP_Settings.KAFKA_HOSTNAME}:{APP_Settings.KAFKA_PORT}")
+
+def Handle_RAW_Topic():
 
     try:
 
         for Message in Kafka_Consumer:
 
             # handle Message.
-            Kafka_Message = json.loads(Message.value.decode())
-
-            # Get Headers
-            Command = Message.headers[0][1].decode('ASCII')
-            Device_ID = Message.headers[1][1].decode('ASCII')
-            Device_IP = Message.headers[2][1].decode('ASCII')
+            #Kafka_Message = json.loads(Message.value.decode())
+            Kafka_Message = Schema.IoT_Data_Pack_Model(**json.loads(Message.value.decode()))
 
             # Print LOG
-            print("Command     : ", Command)
-            print("Device ID   : ", Device_ID)
-            print("Client IP   : ", Device_IP)
+            print("Command     : ", Message.headers[0][1].decode('ASCII'))
+            print("Device ID   : ", Message.headers[1][1].decode('ASCII'))
+            print("Client IP   : ", Message.headers[2][1].decode('ASCII'))
             print(".........................................................")
             print("Topic : ", Message.topic, " - Partition : ", Message.partition, " - Offset : ", Message.offset)
             print(".........................................................")
 
             # Create Add Record Command
             New_Buffer_Post = Models.Incoming_Buffer(
-                Buffer_Device_ID = Device_ID, 
-                Buffer_Client_IP = Device_IP, 
-                Buffer_Command = Command, 
+                Buffer_Device_ID = Message.headers[1][1].decode('ASCII'), 
+                Buffer_Client_IP = Message.headers[2][1].decode('ASCII'), 
+                Buffer_Command = Message.headers[0][1].decode('ASCII'), 
                 Buffer_Data = str(Kafka_Message))
 
             # Add and Refresh DataBase
-            #db = Create_Database()
             db = SessionLocal()
             db.add(New_Buffer_Post)
             db.commit()
             db.refresh(New_Buffer_Post)
 
-            # Close Database
-            db.close()
-
             # Print LOG
             print("Message recorded to Buffer DB with Buffer_ID : ", New_Buffer_Post.Buffer_ID)
             print("---------------------------------------------------------")
+            print("Test : ", Kafka_Message.Device.Info.Hardware)
             print("")
+
+            # Close Database
+            db.close()
 
             # Commit Message
             Kafka_Consumer.commit()
+
+            # Set Message Header
+            Kafka_Message_Headers = [('Command', Message.headers[0][1]), ('ID', Message.headers[1][1]), ('IP', Message.headers[2][1])]
+
+
+
+
+
+
             
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     finally:
         print("Error Accured !!")
 
+
 # Handle All Message in Topic
-Record_Message()
+Handle_RAW_Topic()
 
