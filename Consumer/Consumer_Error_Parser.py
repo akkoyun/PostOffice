ea1bf86d898d91c0e25e09800ec9ffd02859223f
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-08 17:20:23 +0300

    paydentic update

--- Consumer/Consumer_Error_Parser.py
+++ Consumer/Consumer_Error_Parser.py
@@ -23,13 +23,13 @@ def Info_Parser():
         for Message in Kafka_Consumer:
 
             # handle Message.
-            Kafka_Message = json.loads(Message.value.decode())
+            #Kafka_Message = json.loads(Message.value.decode())
 
             # Print LOG
             print(".........................................................")
             print("Topic : ", Message.topic, " - Partition : ", Message.partition, " - Offset : ", Message.offset)
             print(".........................................................")
-            print(Kafka_Message.dict())
+            print(Message.value.decode())
             print(".........................................................")
             print("")
 
