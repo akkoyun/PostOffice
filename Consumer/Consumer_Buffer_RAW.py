f5ae67eb608653fd040ebed85ce0fee8460de9e7
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-08 08:49:38 +0300

    Consumer update

--- Consumer/Consumer_Buffer_RAW.py
+++ Consumer/Consumer_Buffer_RAW.py
@@ -61,8 +61,8 @@ def Record_Message():
         # Commit Message
         TP = TopicPartition(Kafka_Topic, Kafka_Partition)
         OM = OffsetAndMetadata(Kafka_Offset + 1, Kafka_TimeStamp)
-        Kafka_Consumer.commit()
-        Kafka_Consumer.close()
+        Kafka_Consumer.commit(asynchronous=False)
+        #Kafka_Consumer.close()
         #Kafka_Consumer.commit({TP, OM})
 
 # Handle All Message in Topic
