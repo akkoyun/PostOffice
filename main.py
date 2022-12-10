5100c735bf94be3ea0a5d1b24001a241c983a2ba
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-10 11:44:53 +0300

    API update

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 # Library Includes
 from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from config.schema import IoT_Data_Pack_Model
+from config.log_functions import Log_API_Online, Log_API_Offline, Log_API_Error, Log_API_Incomming_Data
 from kafka import KafkaProducer
 from json import dumps
 
 # Define FastAPI Object
 PostOffice = FastAPI()
+
+@PostOffice.on_event("startup")
+async def Startup_Event():
+	Log_API_Online()
+
+@PostOffice.on_event("shutdown")
+async def Shutdown_event():
+	Log_API_Offline()
+
+# Schema Error Handler
+@PostOffice.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+
+	# Defne Kafka Producers
+	Kafka_Producer = KafkaProducer(value_serializer=lambda m: dumps(m).encode('utf-8'), bootstrap_servers="165.227.154.147:9092")
+
+	# Send Message to Queue
+	Kafka_Producer.send("Error", value=str(request))
+
+	# Print LOG
+	Log_API_Error()
+
+	return JSONResponse(
+		status_code=status.HTTP_406_NOT_ACCEPTABLE,
+		content={"Event": 201},
+	)
 
 # IoT Post Method
 @PostOffice.post("/", status_code=status.HTTP_201_CREATED)
@@ -25,7 +54,7 @@ def API(request: Request, Data: IoT_Data_Pack_Model):
 	Kafka_Producer.send(topic='RAW', value=Data.dict(), headers=Kafka_Header)
 
 	# Print LOG
-	print("Incomming Data -->", Data.Payload.TimeStamp, " : ", Data.Device.Info.ID, " - ", Data.Command, " --> Sended to Queue..")
+	Log_API_Incomming_Data(Data.Payload.TimeStamp, Data.Device.Info.ID, Data.Command)
 
 	# Send Success
 	return {"Event": 200}
