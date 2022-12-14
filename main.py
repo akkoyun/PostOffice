8025dfa84645e35991396c3b6a583894059be3ed
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-14 16:01:20 +0300

    ip update

--- main.py
+++ main.py
@@ -43,10 +43,7 @@ def API(request: Request, Data: IoT_Data_Pack_Model):
 	# Defne Kafka Producers
 	Kafka_Producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('utf-8'), bootstrap_servers="165.227.154.147:9092")
 
-	Header = request.headers
-	print(type(Header))
-	print(Header)
-	print(Header['remote_addr'])
+	print(request.headers['remote_addr'])
 
 
 
