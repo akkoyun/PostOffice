085c7d920f4bba74d15307acf6adc311f65475f0
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-14 14:50:40 +0300

    ip update

--- main.py
+++ main.py
@@ -48,7 +48,6 @@ def API(request: Request, Data: IoT_Data_Pack_Model):
 	Header = request.headers
 
 	print(type(Header))
-	print(Header.remote_addr)
 
 	for X in Header:
 		if X[0] == "remote_addr":
