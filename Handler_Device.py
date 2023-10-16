4ab9bf12398ca5b53d980da43831867c169c6f0c
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-10-16 13:03:36 +0300

    Device handler update

--- Handler_Device.py
+++ Handler_Device.py
@@ -3,11 +3,10 @@ from Setup import Database, Models, Log, Schema
 from Setup.Config import APP_Settings
 from kafka import KafkaConsumer
 import json
-import numpy as np
 from datetime import datetime
 
 # Create DB Models
-Database.Base.metadata.create_all(bind=Database.DB_Engine) 
+Database.Base.metadata.create_all(bind=Database.DB_Engine)
 
 # Kafka Consumer
 Kafka_Consumer = KafkaConsumer('RAW',
@@ -16,11 +15,14 @@ Kafka_Consumer = KafkaConsumer('RAW',
                                auto_offset_reset='earliest',
                                enable_auto_commit=False)
 
-# List Finder Function
-def List_Finder(List, Variable):
-    for X in np.array(list(List.__dict__.items())):
-        if X[0] == Variable:
-            return X[1]
+class Headers:
+    def __init__(self, headers):
+        self.Command = headers[0][1].decode('ASCII')
+        self.Device_ID = headers[1][1].decode('ASCII')
+        self.Device_Time = headers[2][1].decode('ASCII')
+        self.Device_IP = headers[3][1].decode('ASCII')
+        self.Size = headers[4][1].decode('ASCII')
+
 
 # Parser Function
 def Device_Handler():
@@ -31,28 +33,20 @@ def Device_Handler():
     try:
         for Message in Kafka_Consumer:
 
-            Raw_Message = Message.value.decode()
-            if not isinstance(Raw_Message, str):
-                Raw_Message = str(Raw_Message)
-            Raw_Message_JSON = json.loads(Raw_Message)
+            Raw_Message_JSON = json.loads(Message.value.decode())
 
             # Handle Message
             Kafka_Message = Schema.Data_Pack_Model(**Raw_Message_JSON)
 
             # Handle Headers
-            class Headers:
-                Command = Message.headers[0][1].decode('ASCII')
-                Device_ID = Message.headers[1][1].decode('ASCII')
-                Device_Time = Message.headers[2][1].decode('ASCII')
-                Device_IP = Message.headers[3][1].decode('ASCII')
-                Size = Message.headers[4][1].decode('ASCII')
+            msg_headers = Headers(Message.headers)
 
             # Database Query
-            Query_Module = DB_Module.query(Models.Device).filter(Models.Device.Device_ID.like(Headers.Device_ID)).first()
+            Query_Module = DB_Module.query(Models.Device).filter(Models.Device.Device_ID.like(msg_headers.Device_ID)).first()
 
             if not Query_Module:
                 New_Module = Models.Module(
-                        Device_ID=Headers.Device_ID,
+                        Device_ID=msg_headers.Device_ID,
                         Device_Data_Count=1,
                         Device_Create_Date=datetime.now(),
                         Device_Last_Online=datetime.now()
@@ -61,19 +55,20 @@ def Device_Handler():
                 DB_Module.commit()
                 DB_Module.refresh(New_Module)
 
+                Module_ID = getattr(New_Module, "Module_ID", None)
+
             else:
-                Module_ID = List_Finder(Query_Module, "Module_ID")
+                Module_ID = getattr(Query_Module, "Module_ID", None)
                 setattr(Query_Module, 'Last_Online_Time', datetime.now())
                 setattr(Query_Module, 'Data_Count', (Query_Module.Data_Count + 1))
 
-                # Öneri 4: Commit işlemi
                 DB_Module.commit()
 
             # Log Message
             Log.Device_Handler_Log(str(Module_ID))
 
     except Exception as e:
-        
+
         # Error Log
         Log.Device_Handler_Error_Log(str(e))
 
