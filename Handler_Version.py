cb75fa2b4a48673bd18350b933c81b391ada7988
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-10-16 14:53:17 +0300

    Device handler update

---
+++ Handler_Version.py
@@ -0,0 +1,42 @@
+# Library Includes
+from Setup import Database, Models, Log, Schema
+from Setup.Config import APP_Settings
+from kafka import KafkaConsumer
+import json
+from datetime import datetime
+
+# Create DB Models
+Database.Base.metadata.create_all(bind=Database.DB_Engine)
+
+# Kafka Consumer
+Kafka_Consumer = KafkaConsumer('RAW',
+                               bootstrap_servers=f"{APP_Settings.POSTOFFICE_KAFKA_HOSTNAME}:{APP_Settings.POSTOFFICE_KAFKA_PORT}",
+                               group_id="Device_Consumer",
+                               auto_offset_reset='earliest',
+                               enable_auto_commit=False)
+
+# Parser Function
+def Version_Handler():
+
+    # Define DB
+    DB_Module = Database.SessionLocal()
+
+    # Handle Messages
+    try:
+        for Message in Kafka_Consumer:
+
+			# handle Message.
+            Kafka_Message = Schema.IoT_Data_Pack_Device(**json.loads(Message.value.decode()))
+
+            # Control for Version
+            if Kafka_Message.Info.Firmware != None or Kafka_Message.Info.Hardware != None:
+
+                print(Kafka_Message.Info.Firmware)
+
+    finally:
+
+        # Close Database
+        DB_Module.close()
+
+# Handle Device
+Version_Handler()
