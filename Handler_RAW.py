e63bb8bf900001dfe8ed5c0fdcb88870a7f1b62f
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-10-19 12:01:30 +0300

    Update

--- Handler_RAW.py
+++ Handler_RAW.py
@@ -18,97 +18,8 @@ Kafka_Consumer = KafkaConsumer('RAW',
 # Kafka Producers
 Kafka_Producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('utf-8'), bootstrap_servers=f'{APP_Settings.POSTOFFICE_KAFKA_HOSTNAME}:{APP_Settings.POSTOFFICE_KAFKA_PORT}')
 
-# Define Incomming Headers
-class Incomming_Headers:
 
-    # Define Incomming Headers
-    def __init__(self, command, device_id, device_time, device_ip, size):
-        
-        # Get Incomming Headers
-        self.Command = command
-        self.Device_ID = device_id
-        self.Device_Time = device_time
-        self.Device_IP = device_ip
-        self.Size = size
 
-# Handle Incomming Headers
-def Handle_Headers(message):
-
-    # Check if all required headers are present    
-    if len(message.headers) >= 5:
-    
-        # Handle Headers
-        headers = Incomming_Headers(
-            message.headers[0][1].decode('ASCII'),
-            message.headers[1][1].decode('ASCII'),
-            message.headers[2][1].decode('ASCII'),
-            message.headers[3][1].decode('ASCII'),
-            message.headers[4][1].decode('ASCII')
-        )
-
-        # Return Headers
-        return headers
-    
-    else:
-
-        # Log Message
-        Log.Device_Header_Handler_Error()
-
-        # Skip to the next iteration
-        return None
-
-# Parse Headers
-def Parse_Headers(Headers, Data_Stream_ID):
-
-    try:
-
-        # Set headers
-        Kafka_Header = [
-            ('Command', bytes(Headers.Command, 'utf-8')), 
-            ('Device_ID', bytes(Headers.Device_ID, 'utf-8')),
-            ('Device_Time', bytes(Headers.Device_Time, 'utf-8')), 
-            ('Device_IP', bytes(Headers.Device_IP, 'utf-8')),
-            ('Size', bytes(Headers.Size, 'utf-8')),
-            ('Data_Stream_ID', bytes(Data_Stream_ID, 'utf-8'))
-        ]
-
-        # Return Kafka Header
-        return Kafka_Header
-
-    except Exception as e:
-
-        # Log Message
-        print(f"An error occurred while setting Kafka headers: {e}")
-        
-        # Return None
-        return None
-
-# Decode and Parse Message
-def Decode_Message(RAW_Message, Kafka_Consumer, Schema):
-    
-    try:
-
-        # Decode Message
-        Decoded_Value = RAW_Message.value.decode()
-        
-        # Parse JSON
-        Parsed_JSON = json.loads(Decoded_Value)
-
-        # Check if JSON is a string
-        if isinstance(Parsed_JSON, str):
-            Parsed_JSON = json.loads(Parsed_JSON)
-        
-        # Get RAW Data
-        Kafka_RAW_Message = Schema.Data_Pack_Model(**Parsed_JSON)
-
-        return Kafka_RAW_Message
-
-    except json.JSONDecodeError:
-        print("JSON Decode Error")
-        return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
 
 # Add DataStream Record
 def DB_Datastream_Add_Record(Headers, DB_Module, Models):
@@ -141,36 +52,6 @@ def DB_Datastream_Add_Record(Headers, DB_Module, Models):
         # Return None
         return None
 
-# Send to Topic
-def Kafka_Send_To_Topic(topic, value, headers, max_retries=3, delay=5):
-
-    # Define Retry Counter
-    Retries = 0
-
-    # Try to Send Message
-    while Retries < max_retries:
-
-        try:
-
-            # Send Message to Queue
-            Kafka_Producer.send(topic, value=value, headers=headers).add_callback(Functions.Kafka_Send_Success).add_errback(Functions.Kafka_Send_Error)
-
-            # Break Loop
-            return
-
-        except Exception as e:
-
-            # Log Message
-            print(f"Failed to send message to {topic}. Attempt {Retries+1} of {max_retries}. Error: {e}")
-
-            # Increment Retry Counter
-            Retries += 1
-
-            # Sleep
-            time.sleep(delay)
-
-    # Log Message
-    print(f"Failed to send message to {topic} after {max_retries} attempts.")
 
 
 
@@ -197,10 +78,10 @@ def Parse_Topics():
         for RAW_Message in Kafka_Consumer:
 
             # Get Headers
-            Headers = Handle_Headers(RAW_Message)
+            Headers = Functions.Handle_Headers(RAW_Message)
 
             # Decode Message
-            Kafka_RAW_Message = Decode_Message(RAW_Message, Kafka_Consumer, Schema)
+            Kafka_RAW_Message = Functions.Decode_Message(RAW_Message, Kafka_Consumer, Schema)
 
             # Control for Decoded Message
             if Kafka_RAW_Message is None:
@@ -217,7 +98,7 @@ def Parse_Topics():
             Kafka_Consumer.commit()
 
             # Set Headers
-            New_Headers = Parse_Headers(Headers, Data_Stream_ID)
+            New_Headers = Functions.Parse_Headers(Headers, Data_Stream_ID)
 
             # Set Topics and Values
             Topics_And_Values = [
@@ -237,7 +118,7 @@ def Parse_Topics():
             for topic, value in Topics_And_Values:
                 
                 # Send to Topic
-                Kafka_Send_To_Topic(topic, value, New_Headers)
+                Functions.Kafka_Send_To_Topic(topic, value, New_Headers)
 
     finally:
 
