029fcca5567a1f77d704dace614777de0aa84c2c
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-10-19 17:12:39 +0300

    Update

--- Default_Value_Update.py
+++ Default_Value_Update.py
@@ -9,16 +9,17 @@ def MNC_Initial_Values():
     
     try:
 
-        # Create New DataStream
-        GSM_MNC_Update = Models.GSM_MNC(
+        # Define MNC Records
+        MNC_Records = [
             Models.GSM_MNC(MNC_ID=1, MNC_Brand_Name="Turkcell", MNC_Operator_Name="Turkcell"),
             Models.GSM_MNC(MNC_ID=2, MNC_Brand_Name="Vodafone", MNC_Operator_Name="Vodafone"),
             Models.GSM_MNC(MNC_ID=3, MNC_Brand_Name="Türk Telekom", MNC_Operator_Name="Türk Telekom")
-        )
+        ]
 
         # Add Record to DataBase
-        DB_Module.add(GSM_MNC_Update)
-
+        for record in MNC_Records:
+            DB_Module.add(record)
+        
         # Commit DataBase
         DB_Module.commit()
 
@@ -35,13 +36,14 @@ def MCC_Initial_Values():
     
     try:
 
-        # Create New DataStream
-        GSM_MCC_Update = Models.GSM_MCC(
+        # Define MCC Records
+        MCC_Records = [
             Models.GSM_MCC(MCC_ID=286, MCC_ISO="TR", MCC_Country_Name="Turkey", MCC_Country_Code=90),
-        )
+        ]
 
         # Add Record to DataBase
-        DB_Module.add(GSM_MCC_Update)
+        for record in MCC_Records:
+            DB_Module.add(record)
 
         # Commit DataBase
         DB_Module.commit()
