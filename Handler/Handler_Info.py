90b128f486b542e3d04494c6b6e7cbbc3cc4dee4
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-10-23 11:46:32 +0300

    Update

--- Handler/Handler_Info.py
+++ Handler/Handler_Info.py
@@ -71,8 +71,8 @@ def Device_Handler():
                 Log.Terminal_Log("INFO", f"Device Updated: {Headers.Device_ID}")
 
             # Get Consumer Record
-            Firmware = Kafka_Info_Message.value['Firmware']
-            Hardware = Kafka_Info_Message.value['Hardware']
+            Firmware = Kafka_Info_Message.Firmware
+            Hardware = Kafka_Info_Message.Hardware
             
             # Database Version Table Query
             Query_Version_Table = DB_Module.query(Models.Version).filter(
