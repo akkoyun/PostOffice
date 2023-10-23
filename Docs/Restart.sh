a0ed7c1e7f9031c269b5d0b62721b701ae97210d
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-10-23 12:47:59 +0300

    Update

---
+++ Docs/Restart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Version: 1.0
+echo "PostOffice System Service Restarting..."
+
+# Restart Deamon
+systemctl daemon-reload
+
+# Restart PostOffice System Service
+systemctl restart PostOffice.service
+
+# Restart RAW Handler
+systemctl restart Handler_RAW.service
+
+# Restart Power Handler
+systemctl restart Handler_Power.service
+
+# Restart Info Handler
+systemctl restart Handler_Info.service
---
+++ Docs/Update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Version: 1.0
+echo "PostOffice System Git Update..."
+
+# Change Directory to PostOffice
+cd /root/PostOffice
+
+# Pull from Git
+git pull
+
+# Version: 1.0
+echo "PostOffice System Service Restarting..."
+
+# Restart Deamon
+systemctl daemon-reload
+
+# Restart PostOffice System Service
+systemctl restart PostOffice.service
+
+# Restart RAW Handler
+systemctl restart Handler_RAW.service
+
+# Restart Power Handler
+systemctl restart Handler_Power.service
+
+# Restart Info Handler
+systemctl restart Handler_Info.service
+
+# Change Directory to Root
+cd ~
