17c0ac6e5ece2d5a71a42639856365b01f3e32ad
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-10-30 15:53:07 +0300

    RAW Handler Update

--- Handler/Handler_RAW.py
+++ Handler/Handler_RAW.py
@@ -28,38 +28,27 @@ def Parse_Topics():
             # Decode Message
             Kafka_RAW_Message = Kafka.Decode_RAW_Message(RAW_Message)
 
-            # Define DataStream ID
-            Data_Stream_ID = 0
+            # Create New DataStream
+            New_Data_Stream = Models.Stream(
+                Device_ID=RAW_Headers.Device_ID,
+                IMEI=Kafka_RAW_Message.Device.IoT.GSM.Module.IMEI,
+                ICCID=Kafka_RAW_Message.Device.IoT.GSM.Operator.ICCID,
+                RSSI=Kafka_RAW_Message.Device.IoT.GSM.Operator.RSSI,
+                Device_IP=RAW_Headers.Device_IP,
+                Connection_Time=Kafka_RAW_Message.Device.IoT.GSM.Operator.ConnTime,
+                Size=RAW_Headers.Size,
+                Create_Date=datetime.now()
+            )
 
-            # Record DataStream
-            try:
+            # Add Record to DataBase
+            DB_Module.add(New_Data_Stream)
 
-                # Create New DataStream
-                New_Data_Stream = Models.Stream(
-                    Device_ID=RAW_Headers.Device_ID,
-                    IMEI=Kafka_RAW_Message.Device.IoT.GSM.Module.IMEI,
-                    ICCID=Kafka_RAW_Message.Device.IoT.GSM.Operator.ICCID,
-                    RSSI=Kafka_RAW_Message.Device.IoT.GSM.Operator.RSSI,
-                    Device_IP=RAW_Headers.Device_IP,
-                    Connection_Time=Kafka_RAW_Message.Device.IoT.GSM.Operator.ConnTime,
-                    Size=RAW_Headers.Size,
-                    Create_Date=datetime.now()
-                )
+            # Commit DataBase
+            DB_Module.commit()
 
-                # Add Record to DataBase
-                DB_Module.add(New_Data_Stream)
+            # Get DataStream ID
+            Data_Stream_ID = New_Data_Stream.Stream_ID
 
-                # Commit DataBase
-                DB_Module.commit()
-
-                # Get DataStream ID
-                Data_Stream_ID = New_Data_Stream.Stream_ID
-
-            except Exception as e:
-                
-                # Log Message
-                print(f"An error occurred while adding DataStream: {e}")
-    
             # Commit Kafka Consumer
             Kafka.Kafka_RAW_Consumer.commit()
 
