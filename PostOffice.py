ea91f6417a7fe646724297c246634199318d381a
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-11-01 15:38:13 +0300

    New Version Update

--- PostOffice.py
+++ PostOffice.py
@@ -210,7 +210,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 	# Create New Stream
 	New_Stream = Models.Stream(
 		Device_ID = "0",
-		ICCID = "0",
+		ICCID = "1",
 		Client_IP = request.client.host,
 		Size = request.headers['content-length'],
 		RAW_Data = request.body,
