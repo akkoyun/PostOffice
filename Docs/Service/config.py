3ad412f8f16c9a8dcead66edbfda2239b26830ea
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-11-02 13:24:30 +0300

    New Version Update

--- Docs/Service/config.py
+++ Docs/Service/config.py
@@ -1,5 +1,11 @@
 # config.py
 import multiprocessing
 
+# Server Socket
 bind = '0.0.0.0:80'
+
+# Workers
 workers = multiprocessing.cpu_count() * 2 + 1
+
+# Worker Class
+worker_class = 'uvicorn.workers.UvicornWorker'
