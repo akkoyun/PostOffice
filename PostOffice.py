df05c5c2f10a7e4fdce26778cfc280aa173b1091
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-11-02 11:21:08 +0300

    New Version Update

--- PostOffice.py
+++ PostOffice.py
@@ -61,7 +61,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 	# Send Error
 	return JSONResponse(
 		status_code=status.HTTP_400_BAD_REQUEST,
-		headers=[{"Server": "PostOffice"}],
+		headers=["Server": "PostOffice"],
 		content={"Event": status.HTTP_400_BAD_REQUEST, "Message": f"{exc}"},
 	)
 
