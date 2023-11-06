e3e0d905552e250032e3071ec178652ed97a05f1
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-11-06 18:07:54 +0300

    New Version Update

--- Routers/WeatherStat.py
+++ Routers/WeatherStat.py
@@ -88,8 +88,8 @@ async def Mobile_App_Root(request: Request, ID: str) -> App_Schema.Model:
 
 	# Parse AT Data
 	if AT_Data is not None:
-		MAX_AT = App_Schema.MaxAT(Value=AT_Data.Max, Time=0)
-		MIN_AT = App_Schema.MinAT(Value=AT_Data.Min, Time=0)
+		MAX_AT = App_Schema.MaxAT(Value=0, Time=0)
+		MIN_AT = App_Schema.MinAT(Value=0, Time=0)
 		AT = App_Schema.AT(Value=AT_Data.Last_Value, Change=AT_Data.Change, AT_FL=AT_FL_Data.Last_Value, AT_Dew=AT_Dew_Data.Last_Value, Max_AT=MAX_AT, Min_AT=MIN_AT)
 
 	# Parse AH Data
