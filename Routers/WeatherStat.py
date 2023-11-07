a4a7253a59fd701a946850b2cd91fc50ccea5468
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-11-07 10:50:48 +0300

    New Version Update

--- Routers/WeatherStat.py
+++ Routers/WeatherStat.py
@@ -62,7 +62,7 @@ async def Mobile_App_Root(request: Request, ID: str) -> App_Schema.Model:
 	if Last_Update is not None:
 
 		# Calculate Time Difference
-		Now = datetime.now()
+		Now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 		TTU = Now - Last_Update
 		TTU_Minutes = (TTU.seconds // 60) % 60
 
