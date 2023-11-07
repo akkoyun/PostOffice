3ceccf20bca19ac774cc55a92c44a496126530b6
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-11-07 10:49:20 +0300

    New Version Update

--- Routers/WeatherStat.py
+++ Routers/WeatherStat.py
@@ -62,10 +62,12 @@ async def Mobile_App_Root(request: Request, ID: str) -> App_Schema.Model:
 	if Last_Update is not None:
 
 		# Calculate Time Difference
-		TTU = datetime.now() - Last_Update
+		Now = datetime.now()
+		TTU = Now - Last_Update
+		TTU_Minutes = (TTU.seconds // 60) % 60
 
 		# Set Device
-		Device = App_Schema.Device(Device_ID = ID, LastUpdate = Handler.Get_Device_Last_Connection(ID).strftime("%Y-%m-%d %H:%M:%S"), TTU = TTU.Minutes)
+		Device = App_Schema.Device(Device_ID = ID, LastUpdate = Handler.Get_Device_Last_Connection(ID).strftime("%Y-%m-%d %H:%M:%S"), TTU = TTU_Minutes)
 
 		# Set Default Values
 		AT = None
