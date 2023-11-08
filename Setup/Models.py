e51e86029fcb8d7493a6d540cb97f8a67e09b446
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-11-08 09:41:05 +0300

    New Version Update

--- Setup/Models.py
+++ Setup/Models.py
@@ -203,26 +203,7 @@ class Service_LOG(Base):
 	Status = Column(Boolean, nullable=False, default=False)
 	Update_Time = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
-# WeatherStat_Measurement Database Model
-class WeatherStat_Measurement(Base):
 
-	# Define Table Name
-	__tablename__ = 'WeatherStat_Measurement' 
-	__table_args__ = {'autoload': True, 'autoload_with': DB_Engine}
-
-	# Define Columns
-	Measurement_ID = Column(Integer, primary_key=True)
-	Device_ID = Column(String())
-	Stream_ID = Column(Integer)
-	Variable = Column(String())
-	Value = Column(Float)
-	Create_Time = Column(TIMESTAMP(timezone=True))
-	Max = Column(Float)
-	Min = Column(Float)
-	Max_Time = Column(TIMESTAMP(timezone=True))
-	Min_Time = Column(TIMESTAMP(timezone=True))
-	PreviousValue = Column(Float)
-	Trend = Column(Integer)
 
 
 
