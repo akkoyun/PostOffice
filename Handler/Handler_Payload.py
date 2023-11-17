d1f4b601ea61d0530e03737e86fdfa0d8f5a0831
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-11-17 13:49:20 +0300

    Type List Function Update

--- Handler/Handler_Payload.py
+++ Handler/Handler_Payload.py
@@ -54,11 +54,14 @@ try:
         # Control for PowerStat Payloads
         for PowerStat_Payload_Name, PowerStat_Payload_Path in Type_List(6000):
 
-            # Get Parameter Path
-            PowerStat_Message_Path = eval(PowerStat_Payload_Path)
+            # Control for WeatherStat Payloads
+            if PowerStat_Message_Path is not None:
 
-            # Handle Parameter
-            Handler.Payload_Recorder(RAW_Headers.Stream_ID, Device_Time, PowerStat_Payload_Name, PowerStat_Message_Path)
+                # Get Parameter Path
+                PowerStat_Message_Path = eval(PowerStat_Payload_Path)
+
+                # Handle Parameter
+                Handler.Payload_Recorder(RAW_Headers.Stream_ID, Device_Time, PowerStat_Payload_Name, PowerStat_Message_Path)
 
         # Commit Kafka Consumer
         Kafka.Payload_Consumer.commit()
