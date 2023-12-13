60a6d9cf337e55214a2d44b0e2425260386bf0df
Mehmet Günce Akkoyun <akkoyun@me.com>
2023-12-13 10:16:27 +0300

    PostOffice Service Update

--- PostOffice.py
+++ PostOffice.py
@@ -125,6 +125,8 @@ async def Data_POST(request: Request, Data: Schema.Data_Pack):
 		("Size", bytes(request.headers['content-length'], 'utf-8')),
 	]
 
+	Log.Terminal_Log("INFO", f"Pack : {Data.dict()}")
+
 	# Send to Kafka Topic
 	Kafka.Send_To_Topic("RAW", Data.json(), Header)
 
