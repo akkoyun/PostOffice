f44ca2a789387f69678b929fe2bbc26cc9e03714
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-07 18:06:31 +0300

    Log Functions Update

--- Setup/Config.py
+++ Setup/Config.py
@@ -2,3 +2,21 @@
 import sys
 sys.path.append('/root/PostOffice/')
 
+# Import Packages
+from pydantic import BaseSettings
+
+# Define Setting
+class Settings(BaseSettings):
+
+	# Server Settings
+	SERVER_NAME: str
+
+	# Logging Settings
+	LOG_FILE: str
+
+	# Load env File
+	class Config:
+		env_file = "Setup/.env"
+
+# Set Setting
+APP_Settings = Settings()
