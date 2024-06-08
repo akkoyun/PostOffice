367f6f61f62b7640276898c1762a8ca0da0483bd
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-08 20:49:56 +0300

    Initial update

--- PostOffice.py
+++ PostOffice.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
-from Functions import Log
+#from Functions import Log
 
 # Define FastAPI Object
 PostOffice = FastAPI(version="02.04.00", title="PostOffice")
@@ -17,7 +17,7 @@ def Main_Root(request: Request):
 	Jinja_ENV = Environment(loader=FileSystemLoader(Templates_Directory))
 
 	# Define the error message
-	Error_Message = f"Hata : İsteğiniz geçersiz. Yardım için destek ekibimize başvurun. [{request.client.host}]"
+	Error_Message = f"Hata: İsteğiniz geçersiz. Yardım için destek ekibimize başvurun. [{request.client.host}]"
 
     # Load the HTML template
 	Template = Jinja_ENV.get_template("HTML_Response.html")
@@ -26,7 +26,7 @@ def Main_Root(request: Request):
 	Rendered_HTML = Template.render(error_message=Error_Message)
 
 	# Log Message
-	Log.Terminal_Log("INFO", f"New Get Request [{request.client.host}]")
+	#Log.Terminal_Log("INFO", f"New Get Request [{request.client.host}]")
 
 	# Return the HTML content
 	return HTMLResponse(content=Rendered_HTML)
