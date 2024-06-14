9446c292b548d6a87e4dcefc1fe7a874c063e463
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-14 14:15:52 +0300

    Post request updates

--- PostOffice.py
+++ PostOffice.py
@@ -126,7 +126,11 @@ async def Data_POST(request: Request, Data: Schema.Data_Pack):
 	Log.Terminal_Log("INFO", f"ICCID     : {Data.Device.IoT.ICCID}")
 	Log.Terminal_Log("INFO", f"IMEI      : {Data.Device.IoT.IMEI}")
 
+	# Get Request Body
+	Request_Body = ((await request.body()).decode("utf-8")).replace(" ", "").replace("\n", "").replace("\r", "")
 
+	# Log Message
+	Log.Terminal_Log("INFO", f"Request Body : {Request_Body}")
 
 
 
