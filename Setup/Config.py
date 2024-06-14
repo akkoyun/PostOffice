9f6f5acb61850dc19d79943726a3b6450229df6f
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-15 02:37:18 +0300

    Schema Update

--- Setup/Config.py
+++ Setup/Config.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('/home/postoffice/PostOffice/src')
 
 # Import Packages
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 # Define Setting
 class Settings(BaseSettings):
