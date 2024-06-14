dd461ecdcb120c2cc5990bbdf444094fe1aa131b
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-14 11:44:44 +0300

    Test procedure update

--- Tests/Data_Post.py
+++ Tests/Data_Post.py
@@ -50,9 +50,9 @@ def test_Data_Post():
 	Response = Test_Client.post("/", json=Data)
 
 	# Print Response
+	print("\r\n")
 	print("--------------------\r\n")
 	print(Response.json())
-	print("\r\n")
 	print("--------------------\r\n")
 
 	# Check Response
