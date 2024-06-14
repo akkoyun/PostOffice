562d8b7e84e5e5772b3a16cd56099b4eae5e7b40
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-15 02:19:58 +0300

    Schema Update

--- Setup/Config.py
+++ Setup/Config.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('/home/postoffice/PostOffice/src')
 
 # Import Packages
-from pydantic_settings import BaseSettings, ConfigDict
+from pydantic_settings import BaseSettings
 
 # Define Setting
 class Settings(BaseSettings):
@@ -42,10 +42,6 @@ class Settings(BaseSettings):
 	# Load env File
 	class Config:
 		env_file = "Setup/.env"
-
-
-    # Load env File
-#    Config = ConfigDict(env_file="Setup/.env")
 
 # Set Setting
 APP_Settings = Settings()
