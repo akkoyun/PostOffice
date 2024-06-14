0b01158f0de069fe347f42ee2730ef3965a42340
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-15 02:06:38 +0300

    Schema Update

--- Setup/Config.py
+++ Setup/Config.py
@@ -3,45 +3,44 @@ import sys
 sys.path.append('/home/postoffice/PostOffice/src')
 
 # Import Packages
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, ConfigDict
 
 # Define Setting
 class Settings(BaseSettings):
 
-	# Server Settings
-	SERVER_NAME: str
-	PROJECT_ROOT: str
+    # Server Settings
+    SERVER_NAME: str
+    PROJECT_ROOT: str
 
-	# Logging Settings
-	LOG_FILE: str
-	LOG_DIR: str
-	LOG_SERVICE_ERROR: str
-	LOG_SERVICE_ACCESS: str
+    # Logging Settings
+    LOG_FILE: str
+    LOG_DIR: str
+    LOG_SERVICE_ERROR: str
+    LOG_SERVICE_ACCESS: str
 
-	# Database Settings
-	DB_HOSTNAME: str
-	DB_PORT: str
-	DB_PASSWORD: str
-	DB_NAME: str
-	DB_USERNAME: str
+    # Database Settings
+    DB_HOSTNAME: str
+    DB_PORT: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    DB_USERNAME: str
 
-	# File Settings
-	FILE_MANUFACTURER: str
-	FILE_MODEL: str
-	FILE_PROJECT: str
-	FILE_GSM_OPERATOR: str
-	FILE_SIM: str
-	FILE_MODEM: str
-	FILE_DATA_SEGMENT: str
-	FILE_MEASUREMENT_TYPE: str
-	FILE_VERSION: str
-	FILE_STATUS: str
-	FILE_DEVICE: str
-	FILE_CALIBRATION: str
+    # File Settings
+    FILE_MANUFACTURER: str
+    FILE_MODEL: str
+    FILE_PROJECT: str
+    FILE_GSM_OPERATOR: str
+    FILE_SIM: str
+    FILE_MODEM: str
+    FILE_DATA_SEGMENT: str
+    FILE_MEASUREMENT_TYPE: str
+    FILE_VERSION: str
+    FILE_STATUS: str
+    FILE_DEVICE: str
+    FILE_CALIBRATION: str
 
-	# Load env File
-	class Config:
-		env_file = "Setup/.env"
+    # Load env File
+    Config = ConfigDict(env_file="Setup/.env")
 
 # Set Setting
 APP_Settings = Settings()
