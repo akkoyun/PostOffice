2d12cb38305374c767f3222b8716db97639307ad
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-19 01:49:53 +0300

    Confluent Kafka Test

--- Functions/Kafka.py
+++ Functions/Kafka.py
@@ -2,31 +2,19 @@
 import sys
 sys.path.append('/home/postoffice/PostOffice/src')
 
-# Import Packages
 from confluent_kafka import Producer
 
-# Define Producer
+p = Producer({'bootstrap.servers': '172.20.5.3:9092'}) 
+
 def delivery_report(err, msg):
-
-    if err is not None:
-
-        print(f'Message delivery failed: {err}')
-
-    else:
-
-        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
-
-# Define Producer
-p = Producer({'bootstrap.servers': '172.20.5.3:9092'})
+	if err is not None:
+		print(f'Message delivery failed: {err}')
+	else:
+		print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
 
 topic = 'Test'
 message = 'Hello, Kafka!'
 
-# Produce Message
 p.produce(topic, message.encode('utf-8'), callback=delivery_report)
-
-# Poll
 p.poll(0)
-
-# Flush
 p.flush()
