745d3347bd261fed07ae62d7619d98ed6a883971
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-20 09:17:21 +0300

    Data Handler Consumer Workup

--- Consumer/DataHandler.py
+++ Consumer/DataHandler.py
@@ -5,6 +5,7 @@ sys.path.append('/home/postoffice/PostOffice/src')
 # Import Libraries
 from Setup.Config import APP_Settings
 from Functions import Log, FastApi_Functions, Database_Functions, Kafka
+from Setup import Database, Models
 from confluent_kafka import Consumer, KafkaException, KafkaError
 import time
 
@@ -24,6 +25,9 @@ RAW_Consumer = Consumer(Consumer_Config)
 
 # Define Subscription Function
 RAW_Consumer.subscribe([RAW_Consumer_Topic])
+
+# Define DB
+DB_Module = Database.SessionLocal()
 
 # Define Consumer Topic Loop
 try:
@@ -60,36 +64,82 @@ try:
 		# Get Message
 		else:
 
-			# Log Message
-			Log.Terminal_Log('INFO', f'Topic : {Consumer_Message.topic()}')
-
-			# Log Message
-			Log.Terminal_Log('INFO', f'-------------------')
-
 			# Get Headers
 			Headers = Consumer_Message.headers()
 
 			# Headers Dict Conversion
 			Headers_Dict = {key: value.decode('utf-8') for key, value in Headers}
 
+			# Get Message Value
+			Message = Consumer_Message.value().decode('utf-8')
+
+			# Define Variables
+			Command_ID = None
+
+			# Check for Command
+			if Headers_Dict['Command'] is not None:
+
+				# Check for Command Table
+				try:
+
+					# Control Service
+					Command_Query = (DB_Module.query(Models.Command).filter(
+						Models.Command.Command.like(Headers_Dict['Command'])
+					).first())
+
+					# Command Found
+					if Command_Query is not None:
+
+						# Get Command ID
+						Command_ID = Command_Query.Command_ID
+
+					else:
+
+						# Log Message
+						Log.Terminal_Log('ERROR', 'Command Not Found!')
+
+						# Set Command ID
+						Command_ID = 0
+
+				finally:
+
+					# Close Database
+					DB_Module.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 			# Log Message
-			Log.Terminal_Log('INFO', f'Command     : {Headers_Dict["Command"]}')
+			Log.Terminal_Log('INFO', f'Topic       : {Consumer_Message.topic()}')
+			Log.Terminal_Log('INFO', f'Command     : {Headers_Dict["Command"]} - [{Command_ID}]')
 			Log.Terminal_Log('INFO', f'Device ID   : {Headers_Dict["Device_ID"]}')
 			Log.Terminal_Log('INFO', f'Device Time : {Headers_Dict["Device_Time"]}')
 			Log.Terminal_Log('INFO', f'Device IP   : {Headers_Dict["Device_IP"]}')
 			Log.Terminal_Log('INFO', f'Size        : {Headers_Dict["Size"]}')
-
-			# Log Message
+			Log.Terminal_Log('INFO', f'-------------------')
+			Log.Terminal_Log('INFO', f'Message     : {Message}')
 			Log.Terminal_Log('INFO', f'-------------------')
 
-			# Get Message Value
-			Message = Consumer_Message.value().decode('utf-8')
 
-			# Log Message
-			Log.Terminal_Log('INFO', f'Message : {Message}')
 
-			# Log Message
-			Log.Terminal_Log('INFO', f'-------------------')
 
 			# Commit Message
 			RAW_Consumer.commit(asynchronous=False)
