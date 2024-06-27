0e9112e276096bb51ab5e9f547abf12dae1ebda5
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-27 14:13:34 +0300

    Rule Service Update

--- Consumer/RuleHandler.py
+++ Consumer/RuleHandler.py
@@ -10,10 +10,10 @@ import time, json, operator
 
 # Define Kafka Consumer
 Rule_Consumer_Config = {
-    'bootstrap.servers': f'{Config.APP_Settings.KAFKA_HOSTNAME}:{Config.APP_Settings.KAFKA_PORT}',
-    'group.id': 'Rule_Handler_Group',
-    'auto.offset.reset': 'earliest',
-    'enable.auto.commit': False,
+	'bootstrap.servers': f'{Config.APP_Settings.KAFKA_HOSTNAME}:{Config.APP_Settings.KAFKA_PORT}',
+	'group.id': 'Rule_Handler_Group',
+	'auto.offset.reset': 'earliest',
+	'enable.auto.commit': False,
 }
 
 # Define Consumer Class
@@ -28,216 +28,231 @@ Log.Terminal_Log('INFO', 'Consumer is starting...')
 # Function to evaluate a single rule
 def Evaluate_Condition(value, condition):
 
-    # Define Operators
-    Operators = {
-        '>': operator.gt,
-        '<': operator.lt,
-        '>=': operator.ge,
-        '<=': operator.le,
-        '==': operator.eq,
-        '!=': operator.ne
-    }
+	# Define Operators
+	Operators = {
 
-    # Check for Condition
-    for Operator_String, Operator_Function in Operators.items():
+		# Arithmetic Operators
+		'>': operator.gt,
+		'<': operator.lt,
+		'>=': operator.ge,
+		'<=': operator.le,
+		'==': operator.eq,
+		'!=': operator.ne
+	}
+
+	# Check for Condition
+	for Operator_String, Operator_Function in Operators.items():
 
 		# Check for Operator
-        if Operator_String in condition:
+		if Operator_String in condition:
 
-            # Check for Value
-            Condition_Value = float(condition.split(Operator_String)[-1].strip())
+			# Check for Value
+			Condition_Value = float(condition.split(Operator_String)[-1].strip())
 
-            # Return Result
-            return Operator_Function(value, Condition_Value)
+			# Return Result
+			return Operator_Function(value, Condition_Value)
 
-    # Return False
-    return False
+	# Return False
+	return False
 
 # Function to evaluate composite rules
-def Evaluate_Composite_Rules(device_id, data, triggered_rules):
+def Evaluate_Composite_Rules(device_id, data):
 
-    # Define Action
-    with Database.SessionLocal() as session:
+	# Define Triggered Rules
+	Triggered_Rules = []
 
-        # Get all main rules
-        Rules = session.query(Models.Rules).all()
+	# Define Action
+	with Database.SessionLocal() as session:
 
-        # Check for Rules
-        for Rule in Rules:
+		# Get all main rules
+		Rules = session.query(Models.Rules).all()
 
-            # Get Rule ID and Action
-            Rule_ID = Rule.Rule_ID
-            Rule_Action = Rule.Rule_Action_ID
-            Rule_Status = Rule.Rule_Status
+		# Check for Rules
+		for Rule in Rules:
 
-            # Get all sub rules
-            Rule_Chains = session.query(Models.Rule_Chain).filter(Models.Rule_Chain.Rule_ID == Rule_ID).all()
+			# Get Rule ID and Action
+			Rule_ID = Rule.Rule_ID
+			Rule_Action = Rule.Rule_Action_ID
+			Rule_Status = Rule.Rule_Status
+			Action_ID = Rule.Rule_Action_ID
 
-            # Define All Conditions Met
-            All_Conditions_Met = True
+			# Get all sub rules
+			Rule_Chains = session.query(Models.Rule_Chain).filter(Models.Rule_Chain.Rule_ID == Rule_ID).all()
 
-            # Check for Rule Chains
-            for Rule_Chain in Rule_Chains:
+			# Define All Conditions Met
+			All_Conditions_Met = True
 
-                # Get Rule Chain Data
-                Rule_Device_ID, Rule_Variable_ID, Rule_Condition = Rule_Chain.Device_ID, Rule_Chain.Variable_ID, Rule_Chain.Rule_Condition
+			# Check for Rule Chains
+			for Rule_Chain in Rule_Chains:
 
-                # Check for Device ID and Variable ID
-                if Rule_Device_ID == device_id and Rule_Status == True and Rule_Variable_ID in data:
+				# Get Rule Chain Data
+				Rule_Device_ID, Rule_Variable_ID, Rule_Condition = Rule_Chain.Device_ID, Rule_Chain.Variable_ID, Rule_Chain.Rule_Condition
 
-                    # Get Value
-                    Value = data[Rule_Variable_ID]
+				# Check for Device ID and Variable ID
+				if Rule_Device_ID == device_id and Rule_Status == True and Rule_Variable_ID in data:
 
-                    # Evaluate Condition
-                    if not Evaluate_Condition(Value, Rule_Condition):
+					# Get Value
+					Value = data[Rule_Variable_ID]
 
-                        # Condition Not Met
-                        All_Conditions_Met = False
-                        break
+					# Evaluate Condition
+					if not Evaluate_Condition(Value, Rule_Condition):
 
-                else:
+						# Condition Not Met
+						All_Conditions_Met = False
 
-                    # Condition Not Met
-                    All_Conditions_Met = False
-                    break
+						# Break
+						break
+				else:
 
-            # Check for All Conditions Met
-            if All_Conditions_Met:
+					# Condition Not Met
+					All_Conditions_Met = False
 
-                # Get Rule for Update
-                Rule_Update = session.query(Models.Rules).filter(Models.Rules.Rule_ID == Rule_ID).first()
+					# Break
+					break
 
-                # Update Rule
-                Rule_Update.Rule_Trigger_Count += 1
+			# Check for All Conditions Met
+			if All_Conditions_Met:
 
-                # Commit Update
-                session.commit()
+				# Get Rule for Update
+				Rule_Update = session.query(Models.Rules).filter(Models.Rules.Rule_ID == Rule_ID).first()
 
-                # Log Rule Triggered
-                Log.Terminal_Log('INFO', f'Rule --> [{device_id}] - [Rule ID: {Rule_ID}] - Action Triggered')
+				# Update Rule
+				Rule_Update.Rule_Trigger_Count += 1
 
-				# Append Triggered Rule
-                triggered_rules.append((device_id, Rule_ID, Rule_Action))
+				# Commit Update
+				session.commit()
+
+				# Log Rule Triggered
+				Log.Terminal_Log('INFO', f'Rule --> [{device_id}] - [Rule ID: {Rule_ID} / Action ID: {Action_ID}] - Action Triggered')
+
+				# Append to Triggered Rules
+				Triggered_Rules.append((device_id, Rule_ID, Rule_Action))
+
+	Log.Terminal_Log('INFO', 'Triggered Rules: ' + str(Triggered_Rules))
+
+	# Return Triggered Rules
+	return Triggered_Rules
 
 # Define Consumer Topic Loop
 try:
 
-    while True:
+	# Loop Consumer
+	while True:
 
-        # Get Message
-        Consumer_Message = Rule_Consumer.poll(timeout=1.0)
+		# Get Message
+		Consumer_Message = Rule_Consumer.poll(timeout=1.0)
 
-        # Check for Message
-        if Consumer_Message is None:
-            continue
+		# Check for Message
+		if Consumer_Message is None:
 
-        # Check for Error
-        if Consumer_Message.error():
-            if Consumer_Message.error().code() == KafkaError._PARTITION_EOF:
-                continue
-            elif Consumer_Message.error():
-                Log.Terminal_Log('ERROR', f'Consumer Error: {Consumer_Message.error()}')
-            continue
+			# Continue
+			continue
 
-        else:
+		# Check for Error
+		if Consumer_Message.error():
 
-			# Check for Message Value
-            try:
-
-                # Decode Message
-                Message = json.loads(Consumer_Message.value().decode('utf-8'))
-
-			# Check for JSON Decode Error
-            except json.JSONDecodeError as e:
-
-				# Log Error
-                Log.Terminal_Log('ERROR', f'JSON Decode Error: {e}')
+			# Check for Error
+			if Consumer_Message.error().code() == KafkaError._PARTITION_EOF:
 
 				# Continue
-                continue
+				continue
 
-            # Get Headers
-            Headers = {key: value.decode('utf-8') for key, value in Consumer_Message.headers()}
+			# Check for Error
+			elif Consumer_Message.error():
 
-            # Get Data Packs
-            Device = Message.get('Device', {})
-            Power_Pack = Device.get('Power', {})
-            IoT_Pack = Device.get('IoT', {})
-            Payload = Message.get('Payload', {})
+				# Log Error
+				Log.Terminal_Log('ERROR', f'Consumer Error: {Consumer_Message.error()}')
 
-            # Define DB
-            DB_Module = Database.SessionLocal()
+			# Continue
+			continue
 
-			# Get Data Types
-            try:
+		# Get Message
+		else:
 
-				# Get Data Types
-                Data_Type_Query = DB_Module.query(Models.Variable).all()
+			# Decode and parse the message
+			try:
 
-				# Format Data
-                Formatted_Data = [(Variable.Variable_ID, Variable.Variable_Unit) for Variable in Data_Type_Query]
+				# Decode Message
+				Message = json.loads(Consumer_Message.value().decode('utf-8'))
 
-            finally:
+			# Check for JSON Decode Error
+			except json.JSONDecodeError as e:
 
-				# Close DB
-                DB_Module.close()
+				# Log Error
+				Log.Terminal_Log('ERROR', f'JSON Decode Error: {e}')
 
-            # Define Found Variables
-            Found_Variables = {}
+				# Continue
+				continue
 
-            # Define Rule Action
-            Action = 0
+			# Get Headers
+			Headers = {key: value.decode('utf-8') for key, value in Consumer_Message.headers()}
 
-            # Check for Tuple and Extract Variable IDs
-            keys_to_check = [var[0] if isinstance(var, tuple) else var for var in Formatted_Data]
+			# Get Data Packs
+			Device = Message.get('Device', {})
+			Power_Pack = Device.get('Power', {})
+			IoT_Pack = Device.get('IoT', {})
+			Payload = Message.get('Payload', {})
 
-            # Function to check variables in a given pack
-            def Check_Variables_in_Pack(pack, pack_name):
-                for variable in keys_to_check:
-                    if variable in pack:
-                        value = pack[variable]
-                        if value is not None and value != "":
-                            Found_Variables[variable] = value
+			# Define DB
+			DB_Module = Database.SessionLocal()
+
+			# Get Pack Dictionary
+			try:
+				
+				# Query all data types
+				Data_Type_Query = DB_Module.query(Models.Variable).all()
+
+				# Get Data Type List
+				Formatted_Data = [(Variable.Variable_ID, Variable.Variable_Unit) for Variable in Data_Type_Query]
+
+			# Check for Error
+			finally:
+
+				# Close Database
+				DB_Module.close()
+
+			# Define Found Variables
+			Found_Variables = {}
+
+			# Define Rule Action
+			Action = 0
+
+			# Check for Tuple and Extract Variable IDs
+			keys_to_check = [var[0] if isinstance(var, tuple) else var for var in Formatted_Data]
+
+			# Function to check variables in a given pack
+			def Check_Variables_in_Pack(pack, pack_name):
+				for variable in keys_to_check:
+					if variable in pack:
+						value = pack[variable]
+						if value is not None and value != "":
+							Found_Variables[variable] = value
 
 			# Check Variables in Packs
-            Check_Variables_in_Pack(Power_Pack, 'Power_Pack')
-            Check_Variables_in_Pack(IoT_Pack, 'IoT_Pack')
-            Check_Variables_in_Pack(Payload, 'Payload')
+			Check_Variables_in_Pack(Power_Pack, 'Power_Pack')
+			Check_Variables_in_Pack(IoT_Pack, 'IoT_Pack')
+			Check_Variables_in_Pack(Payload, 'Payload')
 
-            # Define Triggered Rules
-            Triggered_Rules = []
+			# Evaluate and Log Rules
+			Triggered_Rules = Evaluate_Composite_Rules(Headers['Device_ID'], Found_Variables)
 
-			# Evaluate Composite Rules
-            Evaluate_Composite_Rules(Headers['Device_ID'], Found_Variables, Triggered_Rules)
+			# Log Line
+			Log.Terminal_Log('INFO', '---------------------------------------------------------------')
 
-            # Log Found Variables
-            if not Triggered_Rules:
+			# Commit Message
+			Rule_Consumer.commit(asynchronous=False)
 
-				# Log No Action
-                Log.Terminal_Log('INFO', f'Rule --> [{Headers["Device_ID"]}] - No Action')
-
-            else:
-
-				# Log Triggered Rules
-                for device_id, rule_id, action in Triggered_Rules:
-
-					# Log Action
-                    Log.Terminal_Log('INFO', f'Rule --> [{device_id}] - Rule ID: {rule_id} - Action Triggered: {action}')
-
-            # Commit Message
-            Rule_Consumer.commit(asynchronous=False)
-
-# Handle Keyboard Interrupt
+# Check for Keyboard Interrupt
 except KeyboardInterrupt:
 
-	# Log Shutdown
-    Log.Terminal_Log('INFO', 'Handler is shutting down...')
+	# Consumer Closed Manually
+	Log.Terminal_Log('INFO', 'Handler is shutting down...')
 
-# Handle Finally
+# Check for Finally
 finally:
 
-	# Close Consumer
-    time.sleep(2)
+	# Wait for Finish
+	time.sleep(2)
 
-	# Log Shutdown
-    Rule_Consumer.close()
+	# Close Consumer
+	Rule_Consumer.close()
