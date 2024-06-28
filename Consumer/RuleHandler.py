4bda40d22ad05ac460e00104900bfb72dd2acc3a
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-06-28 10:56:04 +0300

    Rule Service Kafka Update

--- Consumer/RuleHandler.py
+++ Consumer/RuleHandler.py
@@ -241,7 +241,7 @@ try:
 			Log.Terminal_Log('INFO', '---------------------------------------------------------------')
 
 			# Control for Triggered Rules
-			if Triggered_Rules:
+			if Triggered_Rules is not None and len(Triggered_Rules) > 0:
 
 				# Send to Kafka
 				Kafka.Send_To_Topic(Config.APP_Settings.KAFKA_PUBLISH_TOPIC, Triggered_Rules, [('Device_ID', Headers['Device_ID'])])
